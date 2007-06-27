caaa2613513ddcf19f0fb5bb5beb0a739d3bcf0e
Axel Liljencrantz <axel.liljencrantz@freecode.no>
2007-06-27 16:01:32 +0000

    customers/elkjøp/div4c/shield trunk
    Update readme file
    
    Update readme file with information on obtaining and compiling with OCCI.
    
    git-svn-id: https://projects.freecode.no/internal/svn/customers/elkj%C3%B8p/div4c@304 3f367b02-151f-0410-a67f-93a54fc96dc6

--- shield/trunk/database.hh
+++ shield/trunk/database.hh
@@ -1,3 +1,12 @@
+/**
+   \file database.hh
+
+   This file contains an extremely trivial abstraction of a single
+   process-wide database connection. One needs to initialize using the
+   init function to provide username, password, etc. The connection is
+   then not actually created until the first query is executed. If a
+   connection has been created, it is automatically closed on exit.
+*/
 #ifndef DATABASE_HH
 #define DATABASE_HH
 
@@ -7,18 +16,28 @@ using namespace std;
 
 namespace shield
 {
-
-namespace database
-{
   
-  void init (string username, string password, string host);
+  namespace database
+  {
+    
+    /**
+       Call thsi function at atartup to set login information. This
+       function must not be called exactly once.
+    */
+    void init (string username, string password, string host);
+    
+    /**
+       Execute the specified query
+    */
+    bool query (string q, ...);
+    
+    /**
+       Get the last error
+    */
+    string error ();
+    
+  }
   
-  bool query (string q, ...);
-  
-  string error ();
-  
-}
-
 }
 
 #endif
