2292dfda83e54ee91c35038946f7893638afc31c
Axel Liljencrantz <axel.liljencrantz@freecode.no>
2007-10-16 13:45:58 +0000

    customers/elkj%C3%B8p/div4c/shield misc
    Add a few minor utilities for installing Joomal/shield.
    
    Add scripts to perform some minor help task for installing shield.
    
    git-svn-id: https://projects.freecode.no/internal/svn/customers/elkj%C3%B8p/div4c@1045 3f367b02-151f-0410-a67f-93a54fc96dc6

---
+++ shield/misc/import.php
@@ -0,0 +1,70 @@
+#! /bin/env php
+<?php
+/**
+@file import.php
+
+A simple file that reads mysql queries from stdin, separates them on semicolons and executes the individual queries through shield and Oracle.
+
+*/
+
+define ('_VALID_MOS', 1);
+require_once ("database.php");
+
+function execute ($query)
+{
+  echo ".";
+  $res = shield_query ($query);
+  if ($res)
+    {
+      shield_free_result ($res);
+    }
+  else
+    {
+      echo "OH NOES!!!!\n" . shield_error () . "\n\n";
+    }
+  
+}
+
+$stdin = fopen("php://stdin", 'r');
+shield_connect ("localhost", "system", "changeme") || die ("shield_connect");
+shield_select_db ("xe") || die ("shield_select_db");
+$q = false;
+$res = "";
+
+shield_begin_verbose ();
+while (true)
+{
+  $c = fgetc ($stdin);
+
+  if ($c === false)
+    {
+      execute ($res);
+      break;
+    }
+  $res .= $c;
+  
+  if ($q)
+    {
+      if ($c == $q)
+	{
+	  $q = 0;
+	}
+      else if ($c == '\\')
+	{
+	  $c = fgetc ($stdin);
+	  $res .= $c;
+	}
+    }
+  else if ($c == ';')
+    {
+      execute ($res);
+      $res = "";
+    }
+  else if ($c == "'" || $c == '"')
+    {
+      $q = $c;
+    }
+}
+shield_end_verbose ();
+
+?>
