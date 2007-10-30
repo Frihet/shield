880cd09529bb01b6bbd0d320ba7e3434a45241ee
Axel Liljencrantz <axel.liljencrantz@freecode.no>
2007-10-30 11:08:52 +0000

    customers/elkj%C3%B8p/div4c/shield misc
    Code cleanup
    
    Code cleanup in index creation code.
    
    git-svn-id: https://projects.freecode.no/internal/svn/customers/elkj%C3%B8p/div4c@1175 3f367b02-151f-0410-a67f-93a54fc96dc6

--- shield/trunk/src/transform_create_index.cc
+++ shield/trunk/src/transform_create_index.cc
@@ -22,6 +22,27 @@ namespace shield
   namespace transform
   {
 
+    namespace 
+    {
+      
+      void print_insert (ostream &stream, 
+			 string table_name, 
+			 string field_name,
+			 string mangled_table_name,
+			 string index_name)
+      {
+	
+	stream << "insert into shield_table_column (table_name, column_name, "
+	       << "column_type, mangled_table_name, mangled_column_name) values ("
+	       << util::oracle_escape (table_name).first << ", "
+	       << util::oracle_escape (field_name).first << ", " 
+	       << util::oracle_escape ("DATA_TYPE_INDEX").first << ", "
+	       << util::oracle_escape (mangled_table_name).first << "," 
+	       << util::oracle_escape (index_name).first << ")\n\n" << sep;
+      }
+    }
+
+
     create_index::
     create_index ()
       : __table_query (0), __type (PRIMARY_KEY)
@@ -49,34 +70,44 @@ namespace shield
     chain *
     create_index::get_filtered_key_list(chain *field_list)
     {
-      chain *original_key_list = _get_child<chain> (CHILD_KEY_LIST);
+      chain *original_key_list;
+      chain *key_list;
+
+      chain::const_iterator i, j;
+
+      bool ok;
+      string key_name;
+
+      field_spec *spec;
       
+      string field_name;
+
+      original_key_list = _get_child<chain> (CHILD_KEY_LIST);
+
       if (!original_key_list)
 	throw exception::not_found (get_node_name () + ": Could not locate original key list");
 
-      chain *key_list = new chain ();
+      key_list = new chain ();
       key_list->set_separator (",");
       
-      chain::const_iterator i, j;
       
       for (i=original_key_list->begin (); i < original_key_list->end (); i++ )
 	{
-	  bool ok = true;
-
-	  string key_name = (*i)->str ();
+	  ok = true;
+	  key_name = (*i)->str ();
 
 	  for (j=field_list->begin (); j < field_list->end (); j++ )
 	    {
-	      field_spec *s = dynamic_cast<field_spec *> (*j);
+	      spec = dynamic_cast<field_spec *> (*j);
 	      
-	      if (!s)
+	      if (!spec)
 		{
 		  throw exception::invalid_state ("Expected field list item of type field_spec *, got type " + (*j)->get_node_name () + "*");
 		}
 	      
-	      string field_name = s->get_name ()->str ();
+	      field_name = spec->get_name ()->str ();
 	      
-	      if ((field_name == key_name) && (!s->get_type ()->get_indexable ()))
+	      if ((field_name == key_name) && (!spec->get_type ()->get_indexable ()))
 		{
 		  ok = false;
 		  break;
@@ -89,6 +120,7 @@ namespace shield
 	      key_list->push (*i);
 	    }
 	}
+
       if (key_list->size () > 0)
 	{
 	  (*key_list)[0]->set_skip_space (true);
@@ -97,10 +129,17 @@ namespace shield
       return key_list;
     }
 
-    
-    bool key_list_match (chain *key_list1, chain *key_list2)
+    /*
+      Compare two lists of index keys and return true if they are
+      identical (contain the exact same list of elements)
+    */
+    static bool key_list_match (chain *key_list1, chain *key_list2)
     {
       chain::const_iterator i,j;
+
+      string key1;
+      string key2;
+      bool match;
 
       if (key_list1->size () != key_list2->size ())
 	{
@@ -109,14 +148,16 @@ namespace shield
 
       for (i=key_list1->begin (); i<key_list1->end (); i++)
 	{
-	  string k1 = (*i)->str ();
-	  bool match = false;
+	  key1 = (*i)->str ();
+	  match = false;
+
 	  for (j=key_list2->begin (); j<key_list2->end (); j++)
 	    {
-	      string k2 = (*j)->str ();
-	      if (k1 == k2)
+	      key2 = (*j)->str ();
+	      if (key1 == key2)
 		{
 		  match = true;
+		  break;
 		}	  
 	    }
 
@@ -133,19 +174,31 @@ namespace shield
     _print (ostream &stream)
     {
 
+      chain *field_list;
+      printable *table_name;
+      chain *key_list;
+
+      vector<printable *>::const_iterator pii;
+
+      string f_name, t_name, name;
+
+      printable *field_name;
+
+
       if (!_get_child (CHILD_KEY_LIST))
 	throw shield::exception::not_found ("create_index node with no key list");
       
       if (!__table_query)
 	throw shield::exception::not_found ("create_index::_print called without first calling internal_transform ()");
 
-      chain *field_list = __table_query->get_field_list ();
+      field_list = __table_query->get_field_list ();
       
-      printable *table_name = __table_query->get_name ();
+      table_name = __table_query->get_name ();
+      
       if (!table_name)
 	throw exception::invalid_state (get_node_name () + ": table name not set");
       
-      chain *key_list = get_filtered_key_list (field_list);
+      key_list = get_filtered_key_list (field_list);
       
       if (!key_list->size ())
 	{
@@ -156,8 +209,6 @@ namespace shield
 	}
       
       vector<printable *> &prev_index = __table_query->get_prev_index_list ();
-      
-      vector<printable *>::const_iterator pii;
 
       for (pii = prev_index.begin (); pii < prev_index.end (); pii++)
 	{
@@ -193,16 +244,16 @@ namespace shield
 	      throw exception::syntax ("Unnamed non-primary keys are not supported!");
 	    }
 	  
-	  string t_name = table_name->unmangled_str ();
-	  printable *field_name = _get_child (CHILD_NAME);
+	  t_name = table_name->unmangled_str ();
+	  field_name = _get_child (CHILD_NAME);
 	  
 	  if (!field_name)
 	    {
 	      throw exception::invalid_state ("Field for index is not of type text");
 	    }
 
-	  string f_name = field_name->unmangled_str ();
-	  string name = t_name + "_" + f_name;
+	  f_name = field_name->unmangled_str ();
+	  name = t_name + "_" + f_name;
       
 	  if (name.length () > 30)
 	    {
@@ -220,22 +271,11 @@ namespace shield
 	  stream << " index " << name << endl;
 	  stream << "on" << *table_name << " (" << *key_list << ")";
 	  stream  << endl << endl << sep;
-
-	  string str = "insert into shield_table_column (table_name, column_name, column_type, mangled_table_name, mangled_column_name) values (";
-      
-	  str += util::oracle_escape (table_name->unmangled_str ()).first+", "
-	    + util::oracle_escape (field_name->unmangled_str ()).first + ", " 
-	    + util::oracle_escape ("DATA_TYPE_INDEX").first + ", "
-	    + util::oracle_escape (table_name->str ()).first + "," 
-	    + util::oracle_escape (name).first 
-	    + ")\n\n";
 	  
-	  stream << str << sep;
-
+	  print_insert( stream, table_name->unmangled_str (), field_name->unmangled_str (), table_name->str (), name);
+	  
 	}
   
-
-
       cache::clear ();
     }
 
